e391d30f02d1edabc12c07f348a2f8169369523a
Stian Lødemel <598993@stud.hvl.no>
2021-09-20 17:32:53 +0200

    lagt til oppgave c

--- src/no/hvl/dat100/O1A.java
+++ src/no/hvl/dat100/O1A.java
@@ -8,6 +8,10 @@ public class O1A {
 		skrivUt(heltallsTabell);
 		String tabellTilStreng = tilstreng(heltallsTabell);
 		System.out.println(tabellTilStreng);
+
+		System.out.println(summerForLøkke(heltallsTabell));
+		System.out.println(summerWhileLøkke(heltallsTabell));
+		System.out.println(summerForEach(heltallsTabell));
 	}
 
 	// a)
@@ -25,18 +29,51 @@ public class O1A {
 
 	}
 
-	
-	// b) 
-	
+	// b)
+
 	public static String tilstreng(int[] tabell) {
-		
+
 		String nyStreng = "[";
-		for(int i =0;i<tabell.length-1;i++) {
+		for (int i = 0; i < tabell.length - 1; i++) {
 			nyStreng = nyStreng + tabell[i] + ",";
 		}
-		nyStreng = nyStreng + tabell[tabell.length-1];
+		nyStreng = nyStreng + tabell[tabell.length - 1];
 		nyStreng = nyStreng + "]";
-		
+
 		return nyStreng;
+	}
+
+	// c
+
+	public static int summerForLøkke(int[] tabell) {
+
+		int sum = 0;
+
+		for (int i = 0; i < tabell.length; i++) {
+			sum = sum + tabell[i];
+		}
+		return sum;
+	}
+
+	public static int summerWhileLøkke(int[] tabell) {
+
+		int sum = 0;
+		int i = 0;
+		while (i < tabell.length) {
+
+			sum += tabell[i];
+			i++;
+		}
+
+		return sum;
+	}
+
+	public static int summerForEach(int[] tabell) {
+
+		int sum = 0;
+		for (int tall : tabell) {
+			sum += tall;
+		}
+		return sum;
 	}
 }
