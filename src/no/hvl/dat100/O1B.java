33981c6918811824aa86b55ec7500314eade9143
Stian Lødemel <598993@stud.hvl.no>
2021-09-30 21:51:44 +0200

    lagt til oppgave c)

--- src/no/hvl/dat100/O1B.java
+++ src/no/hvl/dat100/O1B.java
@@ -3,26 +3,42 @@ package no.hvl.dat100;
 public class O1B {
 
 	public static void main(String[] args) {
-	int[][] test = {
-			{2,4,45,2},
-			{6,2,6,2},
-	};
-	
-	skrivUt1(test);
+		int[][] test = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		skrivUt1(test);
+		System.out.println(tilStreng(test));
 
 	}
 // a)
-	
+
 	public static void skrivUt1(int[][] matrise) {
-		
-		
-		
-		for(int rad[] : matrise) {
-			for(int element : rad) {
+
+		for (int rad[] : matrise) {
+			for (int element : rad) {
 				System.out.print(element + " ");
 			}
 			System.out.println();
 		}
-	
+
 	}
+
+	// b)
+
+	public static String tilStreng(int[][] matrise) {
+		String returStreng = "";
+
+		for (int i = 0; i < matrise.length; i++) {
+			int[] rad = matrise[i];
+			for (int k = 0; k < rad.length; k++) {
+				returStreng += rad[k] + " ";
+			}
+			returStreng += "\n";
+		}
+		return returStreng;
+	}
+	
+	// c)
+	
+	
+
 }
